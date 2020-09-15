5994a5da7f472c7717291e4e9291d6a83fff7e84
Jaswitha Reddy <jaswithareddyguntaka@gmail.com>
2020-09-15 13:57:49 +0530

    Update four.py

--- four.py
+++ four.py
@@ -4,6 +4,8 @@
 Created on Sat Sep  5 16:50:13 2020
 
 @author: jaswithareddy
+
+Encrypt a plain text using one to one mapping.
 """
 
 mapping={'A':'A','B':'N','C':'D','D':'R','E':'E','F':'W','G':'I','H':'C','I':'K','J':'S','K':'O','L':'H','M':'T','N':'B','O':'F','P':'G','Q':'J','R':'L','S':'M','T':'P','U':'Q','V':'U','W':'V','X':'X','Y':'Y','Z':'Z'}
