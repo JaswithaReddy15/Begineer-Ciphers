e2e47ca91de3c854be891d488ac38a4cee5733a1
Jaswitha Reddy <jaswithareddyguntaka@gmail.com>
2020-09-15 14:08:56 +0530

    Update ten.py

--- ten.py
+++ ten.py
@@ -4,6 +4,8 @@
 Created on Wed Sep  9 17:37:39 2020
 
 @author: jaswithareddy
+
+Encrypt and Decrypt text using Vigenère Cipher.
 """
 
 alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
